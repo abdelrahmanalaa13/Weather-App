0509a2820785cfc70ca7dc73425f2b5d08dc7e75
Abdelrahman Alaa <abdelrahman.alaa@widebot.ai>
2021-05-21 19:54:59 +0200

    Setting base style weather main details section

--- src/app/modules/weather/weather.module.ts
+++ src/app/modules/weather/weather.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { WeatherHomeComponent } from './components/weather-home/weather-home.component';
 import { WeatherRoutingModule } from './weather-routing.module';
 import { WeatherHeaderComponent } from './components/weather-header/weather-header.component';
+import { WeatherDetailsComponent } from './components/weather-details/weather-details.component';
 
 @NgModule({
-  declarations: [WeatherHomeComponent, WeatherHeaderComponent],
+  declarations: [WeatherHomeComponent, WeatherHeaderComponent, WeatherDetailsComponent],
   imports: [CommonModule, WeatherRoutingModule],
 })
 export class WeatherModule {}
