082a3cb5d4446025b733a29a6eaf288ad7aa2497
Abdelrahman Alaa <abdelrahman.alaa@widebot.ai>
2021-05-21 18:06:43 +0200

    Setting base style with home and header

--- src/app/modules/weather/weather.module.ts
+++ src/app/modules/weather/weather.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WeatherHomeComponent } from './components/weather-home/weather-home.component';
 import { WeatherRoutingModule } from './weather-routing.module';
+import { WeatherHeaderComponent } from './components/weather-header/weather-header.component';
 
 @NgModule({
-  declarations: [WeatherHomeComponent],
+  declarations: [WeatherHomeComponent, WeatherHeaderComponent],
   imports: [CommonModule, WeatherRoutingModule],
 })
 export class WeatherModule {}
